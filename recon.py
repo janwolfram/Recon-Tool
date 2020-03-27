5939285ce056dccae609ad3e01fc87ad83d9e71d
wolfram <s6jnwolf@uni-bonn.de>
2020-03-28 00:11:42 +0100

    main method

--- recon.py
+++ recon.py
@@ -4,16 +4,22 @@
 import requests
 import pprint
 
-res = requests.get('http://localhost:5000/rest/firmware/950f6f50fa2a16295193a9586bf6795b722805ab32099f5631f35b3fc717ce1b_33228800')
 
-res = res.json()
-pp = pprint.PrettyPrinter(indent=0, width=80, depth=None)
-pp.pprint(res)
+def main():
+    res = requests.get('http://localhost:5000/rest/firmware/950f6f50fa2a16295193a9586bf6795b722805ab32099f5631f35b3fc717ce1b_33228800')
+
+    res = res.json()
+    pp = pprint.PrettyPrinter(indent=0, width=80, depth=None)
+    pp.pprint(res)
+    #print(res)
 
 
+    # hier kommt rekursive funktion um dict tiefgehend durchzugehen
+    '''for key, value in res.items():
+        print(key, value)
+        print("\n")
+    '''
 
-# hier kommt rekursive funktion um dict tiefgehend durchzugehen
-'''for key, value in res.items():
-    print(key, value)
-    print("\n")'''
+if __name__ == '__main__':
+    main()
 
