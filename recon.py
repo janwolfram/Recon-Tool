47f4bddb4d8cb18f4992da6bc9aac15cd461ec4d
wolfram <s6jnwolf@uni-bonn.de>
2020-03-27 23:06:02 +0100

    added file

---
+++ recon.py
@@ -0,0 +1,19 @@
+# sudo pip install requests
+# python3 recon.py
+
+import requests
+import pprint
+
+res = requests.get('http://localhost:5000/rest/firmware/950f6f50fa2a16295193a9586bf6795b722805ab32099f5631f35b3fc717ce1b_33228800')
+
+res = res.json()
+pp = pprint.PrettyPrinter(indent=0, width=80, depth=None)
+pp.pprint(res)
+
+
+
+# hier kommt rekursive funktion um dict tiefgehend durchzugehen
+'''for key, value in res.items():
+    print(key, value)
+    print("\n")'''
+
