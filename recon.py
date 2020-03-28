799a1e9ceb006715036a6528839e6e2931ee8454
wolfram <s6jnwolf@uni-bonn.de>
2020-03-28 23:48:15 +0100

    better imports

--- recon.py
+++ recon.py
@@ -2,26 +2,26 @@
 # python3 recon.py
 # pretty print: python3 recon.py | python3 -m json.tool
 
-import requests
-import json
-import copy
+from requests import get
+from json import dumps
+from copy import deepcopy
 
 
 def findComponent(tree, component):
     for key, value in tree.items():
         if isinstance(value, dict):
             if key == component:
-                componentDict = json.dumps({component : copy.deepcopy(value)})
+                componentDict = dumps({component : deepcopy(value)})
                 print(componentDict)
             findComponent(value, component)
         else:
             if key == component:
-                componentDict = json.dumps({component : value})
+                componentDict = dumps({component : value})
                 print(componentDict)
 
 
 def main():
-    res = requests.get('http://localhost:5000/rest/firmware/950f6f50fa2a16295193a9586bf6795b722805ab32099f5631f35b3fc717ce1b_33228800').json()
+    res = get('http://localhost:5000/rest/firmware/950f6f50fa2a16295193a9586bf6795b722805ab32099f5631f35b3fc717ce1b_33228800').json()
     findComponent(res, "software_components")
 
 
