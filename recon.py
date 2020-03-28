ece3526a3c934caadf265984b2eb5147a50bebdc
wolfram <s6jnwolf@uni-bonn.de>
2020-03-28 23:42:44 +0100

    test commit

--- recon.py
+++ recon.py
@@ -7,13 +7,13 @@ import json
 import copy
 
 
-def FindComponent(tree, component): 
+def findComponent(tree, component):
     for key, value in tree.items():
         if isinstance(value, dict):
             if key == component:
                 componentDict = json.dumps({component : copy.deepcopy(value)})
                 print(componentDict)
-            FindComponent(value, component)
+            findComponent(value, component)
         else:
             if key == component:
                 componentDict = json.dumps({component : value})
@@ -22,7 +22,7 @@ def FindComponent(tree, component):
 
 def main():
     res = requests.get('http://localhost:5000/rest/firmware/950f6f50fa2a16295193a9586bf6795b722805ab32099f5631f35b3fc717ce1b_33228800').json()
-    FindComponent(res, "analysis")
+    findComponent(res, "software_components")
 
 
 if __name__ == '__main__':
