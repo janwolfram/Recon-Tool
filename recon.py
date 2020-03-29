579e4831b05f47a2ee6ed405ade2500c545c22ab
wolfram <s6jnwolf@uni-bonn.de>
2020-03-29 13:33:39 +0200

    setup argparse

--- recon.py
+++ recon.py
@@ -1,30 +1,38 @@
-# sudo pip install requests
+# sudo pip install requests whaaaaat
 # python3 recon.py
 # pretty print: python3 recon.py | python3 -m json.tool
 
 from requests import get
 from json import dumps
 from copy import deepcopy
+import argparse
 
 
 def findComponent(tree, component):
     for key, value in tree.items():
         if isinstance(value, dict):
             if key == component:
-                componentDict = dumps({component : deepcopy(value)})
+                componentDict = dumps({component: deepcopy(value)})
                 print(componentDict)
             findComponent(value, component)
         else:
             if key == component:
-                componentDict = dumps({component : value})
+                componentDict = dumps({component: value})
                 print(componentDict)
 
 
+def setupArgparse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("uid", help="uid of firmware")
+    parser.add_argument("component", help="component you are looking for")
+    return parser.parse_args()
+
+
 def main():
-    res = get('http://localhost:5000/rest/firmware/950f6f50fa2a16295193a9586bf6795b722805ab32099f5631f35b3fc717ce1b_33228800').json()
-    findComponent(res, "software_components")
+    args = setupArgparse()
+    res = get('http://localhost:5000/rest/firmware/' + args.uid).json()
+    findComponent(res, args.component)
 
 
 if __name__ == '__main__':
     main()
-
