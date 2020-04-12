9bdc95808e6a2604e03ecd381fedac9f5415a203
Wolfram <s6jnwolf@uni-bonn.de>
2020-04-12 14:19:24 +0200

    schöneres Design

--- recon.py
+++ recon.py
@@ -42,8 +42,23 @@ def main():
     json['crypto_material'] = crypto
 
 
+    software_components = getSoftwareComponents(res)
+    sc = {}
+    for key in software_components:
+        arr = []
+        liste = getSoftwareComponents(res)[key]
+        for e in liste:
+            f = get('http://localhost:5000/rest/file_object/' + e).json()
+            arr.append(
+                {'name': getHid(f).split("/")[-1], 'uid': e, 'exploit_mitigations': {'Canary': getExploitMitigation(f, 'Canary'),
+                                                        'NX': getExploitMitigation(f, 'NX'),
+                                                        'PIE': getExploitMitigation(f, 'PIE'),
+                                                        'RELRO': getExploitMitigation(f, 'RELRO')}}
+            )
+        sc[key] = arr
 
-    json['software_components'] = getSoftwareComponents(res)
+    json['software_components'] = sc
+    #json['software_components'] = getSoftwareComponents(res)
 
     index_filesystems = []
     for i, key in enumerate(list(getFileType(res).keys())):
@@ -75,8 +90,57 @@ def main():
     # print(json)
     # findConfigs(loads(dumps(json["whitelist"])))
 
-    print(colored('[+]', 'green'), colored('device_name', 'blue'))
-    print(json['crypto_material']['SSLCertificate'][0]['material'][0])
+    print(colored('[+]', 'green'), colored('device_name:', 'blue'), getDeviceName(res))
+    print('--------------------------------------------------------')
+    print(colored('[+]', 'green'), colored('crypto_material:\n', 'yellow'))
+    for key in json['crypto_material']:
+        print(colored('[+]', 'green'), colored('key:', 'blue'), colored(key, 'red'))
+        print(colored('--------------------------------------------------------', 'red'))
+        liste = json['crypto_material'][key]
+        for e in liste:
+            print(colored('\n[+]', 'green'), colored('name:', 'blue'), e['name'])
+            print(colored('[+]', 'green'), colored('uid:', 'blue'), e['uid'])
+            for material in e['material']:
+                print('\n' + material)
+    # print(json['crypto_material']['SSLCertificate'][0]['material'][0])
+
+    print(colored('\n[+]', 'green'), colored('software_components:\n', 'yellow'))
+    for key in json['software_components']:
+        print(colored('--------------------------------------------------------', 'red'))
+        print(colored('[+]', 'green'), colored('key:', 'blue'), colored(key + '\n', 'red'))
+        liste = json['software_components'][key]
+        for i, e in enumerate(liste):
+            print(colored('[+]', 'green'), colored('name:', 'blue'), e['name'])
+            print(colored('[+]', 'green'), colored('uid:', 'blue'), e['uid'])
+            print(colored('[+]', 'green'), colored('exploit_mitigations:', 'blue'))
+            print(colored('    ->', 'green'), colored('Canary:', 'blue'), e['exploit_mitigations']['Canary'])
+            print(colored('    ->', 'green'), colored('NX:', 'blue'), e['exploit_mitigations']['NX'])
+            print(colored('    ->', 'green'), colored('PIE:', 'blue'), e['exploit_mitigations']['PIE'])
+            print(colored('    ->', 'green'), colored('RELRO:', 'blue'), e['exploit_mitigations']['RELRO'])
+            if i != len(liste) - 1:
+                print(colored('--------------------------------------------------------', 'blue'))
+        print(colored('--------------------------------------------------------\n', 'red'))
+
+    print(colored('\n[+]', 'green'), colored('whitelist:\n', 'yellow'))
+    for key in json['whitelist']:
+        print(colored('--------------------------------------------------------', 'red'))
+        liste = json['whitelist'][key]
+        if len(liste) > 0:
+            print(colored('[+]', 'green'), colored('key:', 'blue'), colored(key + '\n', 'red'))
+        else:
+            print(colored('[-]', 'red'), colored('key:', 'blue'), colored(key, 'red'))
+        for i, e in enumerate(liste):
+            print(colored('[+]', 'green'), colored('name:', 'blue'), e['name'])
+            print(colored('[+]', 'green'), colored('uid:', 'blue'), e['uid'])
+            print(colored('[+]', 'green'), colored('exploit_mitigations:', 'blue'))
+            print(colored('    ->', 'green'), colored('Canary:', 'blue'), e['exploit_mitigations']['Canary'])
+            print(colored('    ->', 'green'), colored('NX:', 'blue'), e['exploit_mitigations']['NX'])
+            print(colored('    ->', 'green'), colored('PIE:', 'blue'), e['exploit_mitigations']['PIE'])
+            print(colored('    ->', 'green'), colored('RELRO:', 'blue'), e['exploit_mitigations']['RELRO'])
+            if i != len(liste) - 1:
+                print(colored('--------------------------------------------------------', 'blue'))
+        print(colored('--------------------------------------------------------\n', 'red'))
+
 
     print(f'Time taken: {time() - start_first}')
 
